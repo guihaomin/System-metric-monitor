80999bfba39291c96e631365e951c3e0ffe58938
guihaomin <gui.29@osu.edu>
2017-09-14 09:53:43 +0800

    some comment

--- projCPUUtil.py
+++ projCPUUtil.py
@@ -9,7 +9,7 @@ total=[]
 cpuSum2=[]
 total2=[]
 parse=a[i].split(' ')
-while(parse[0]!='intr'):
+while(parse[0]!='intr'): #get initial info
    parse=a[i].split()
    cpuSum.append(int(parse[1])+int(parse[2])+int(parse[3])+int(parse[5])+int(parse[6])+int(parse[7])+int(parse[8])+int(parse[9]))
    
@@ -21,10 +21,12 @@ proc=open("/proc/stat","r")
 a=proc.readlines()
 proc.close()
 parse=a[0].split(' ')
-while(parse[0]!='intr'):
+while(parse[0]!='intr'): #get info after 5s to compute change
    parse=a[i].split()
    cpuSum2.append(int(parse[1])+int(parse[2])+int(parse[3])+int(parse[5])+int(parse[6])+int(parse[7])+int(parse[8])+int(parse[9]))
    total2.append(int(parse[4])+cpuSum2[i])
    i+=1
-for t in range(i-1):
+print("Number of cores is %d" % (i-2))
+print("first is overall usage then usage of core")
+for t in range(i-1):#first is overall usage then usage of cores
    print(100.0*(cpuSum2[t]-cpuSum[t])/(total2[t]-total[t]))
