5ce4a5120c77eee6ab16b0d3417894c46e437eda
guihaomin <gui.29@osu.edu>
2017-09-25 13:01:49 -0400

    add known issue that need fix

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import time
 import linux_metrics as lm
 def assemble(name,value,unit,interval,isCumulative,transform,description="NIL"):
     return "%s %f %s %d %s %s %s" % (name,value,unit,interval,isCumulative,transform,description)
-refresh_interval=15
+refresh_interval=5
 interval=5
 logger=logging.getLogger()
 formatter=logging.Formatter('%(asctime)s %(message)s')
@@ -12,7 +12,7 @@ file_handler=logging.FileHandler("metric.log")
 logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
-met=lm.integrated.metric(interval,'sda',"wlp2s0")
+met=lm.integrated.metric(interval,'sda',"eno1")
 while True:
     print("sleeping...")
     time.sleep(refresh_interval)
